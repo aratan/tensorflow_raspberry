f45c2fd82c6a9d3ec0bc41105c19ed849ce743fa
aratan <victor.arbiol@gmail.com>
2017-12-02 14:59:37 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#I want to thank lhelontra and recognize you for sharing the package tensorflow-1.4.0-cp35-none-linux_armv7l.whl
+wget https://github.com/lhelontra/tensorflow-on-arm/releases/download/v1.4.0/tensorflow-1.4.0-cp35-none-linux_armv7l.whl
+sudo pip3 install tensorflow-1.4.0-cp35-none-linux_armv7l.whl
+#extras in python3
+sudo pip3 install -U scikit-learn
+sudo pip3 install -U matplotlib
+sudo pip3 install numpy
+sudo pip3 install scipy
