cf8b48382d262085fafdbc58d9120bb41cca66a8
aratan <victor.arbiol@gmail.com>
2017-12-11 08:31:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #I want to thank lhelontra and recognize you for sharing the package tensorflow-1.4.0-cp35-none-linux_armv7l.whl
 wget https://github.com/lhelontra/tensorflow-on-arm/releases/download/v1.4.0/tensorflow-1.4.0-cp35-none-linux_armv7l.whl
+sudo apt install python3-pip -y
 sudo pip3 install tensorflow-1.4.0-cp35-none-linux_armv7l.whl
 #extras in python3
 sudo pip3 install -U scikit-learn
